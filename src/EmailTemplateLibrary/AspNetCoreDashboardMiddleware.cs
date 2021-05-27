8bb95c7b1313056474c9a1c00c0d734bdb13d30f
Salie Hendricks <saliehendricks@gmail.com>
2021-05-27 08:58:00 +0200

    Add project files.

---
+++ src/EmailTemplateLibrary/AspNetCoreDashboardMiddleware.cs
@@ -0,0 +1,85 @@
+﻿using EmailTemplateLibrary.Dashboard;
+using EmailTemplateLibrary.Storage;
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailTemplateLibrary
+{
+    public class AspNetCoreDashboardMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly DashboardOptions _options;
+        private readonly RouteCollection _routes;
+        private readonly TemplateStorage _storage;
+        
+        public AspNetCoreDashboardMiddleware(
+            RequestDelegate next,
+            TemplateStorage storage,
+            DashboardOptions options,
+            RouteCollection routes)
+        {
+            if (next == null) throw new ArgumentNullException(nameof(next));
+            if (storage == null) throw new ArgumentNullException(nameof(storage));
+            if (options == null) throw new ArgumentNullException(nameof(options));
+            if (routes == null) throw new ArgumentNullException(nameof(routes));
+
+            _next = next;
+            _storage = storage;
+            _options = options;
+            _routes = routes;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var context = new AspNetCoreDashboardContext(_storage, _options, httpContext);
+            var findResult = _routes.FindDispatcher(httpContext.Request.Path.Value);
+
+            if (findResult == null)
+            {
+                await _next.Invoke(httpContext);
+                return;
+            }
+
+            foreach (var filter in _options.Authorization)
+            {
+                if (!filter.Authorize(context))
+                {
+                    var isAuthenticated = httpContext.User?.Identity?.IsAuthenticated;
+
+                    httpContext.Response.StatusCode = isAuthenticated == true
+                        ? (int)HttpStatusCode.Forbidden
+                        : (int)HttpStatusCode.Unauthorized;
+
+                    return;
+                }
+            }
+
+            if (!_options.IgnoreAntiforgeryToken)
+            {
+                var antiforgery = httpContext.RequestServices.GetService<IAntiforgery>();
+
+                if (antiforgery != null)
+                {
+                    var requestValid = await antiforgery.IsRequestValidAsync(httpContext);
+
+                    if (!requestValid)
+                    {
+                        // Invalid or missing CSRF token
+                        httpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                        return;
+                    }
+                }
+            }
+
+            context.UriMatch = findResult.Item2;
+
+            await findResult.Item1.Dispatch(context);
+        }
+    }
+}
+
---
+++ src/EmailTemplateLibrary/Dashboard/DashboardRoutes.cs
@@ -0,0 +1,30 @@
+﻿using EmailTemplateLibrary.Dashboard.Pages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EmailTemplateLibrary.Dashboard
+{
+    public static class DashboardRoutes
+    {
+        public static RouteCollection Routes { get; }
+        static DashboardRoutes()
+        {
+            Routes = new RouteCollection();
+            Routes.AddRazorPage("/", x => new HomePage());
+        }
+
+        public static void AddRazorPage(
+            this RouteCollection routes,
+            string pathTemplate,
+            Func<Match, RazorPage> pageFunc)
+        {
+            if (routes == null) throw new ArgumentNullException(nameof(routes));
+            if (pathTemplate == null) throw new ArgumentNullException(nameof(pathTemplate));
+            if (pageFunc == null) throw new ArgumentNullException(nameof(pageFunc));
+
+            routes.Add(pathTemplate, new RazorPageDispatcher(pageFunc));
+        }
+    }
+}

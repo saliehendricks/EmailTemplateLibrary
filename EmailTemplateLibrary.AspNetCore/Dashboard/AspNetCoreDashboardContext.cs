22977ff73d8fed3cba2224b44b8a2a09c6c3ea0e
Salie Hendricks <saliehendricks@gmail.com>
2021-05-31 09:20:54 +0200

    AspnetCore dependency separated for impr compat

---
+++ EmailTemplateLibrary.AspNetCore/Dashboard/AspNetCoreDashboardContext.cs
@@ -0,0 +1,40 @@
+﻿using EmailTemplateLibrary.Dashboard;
+using EmailTemplateLibrary.Storage;
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace EmailTemplateLibrary.AspNetCore.Dashboard
+{
+    public class AspNetCoreDashboardContext : DashboardContext
+    {
+        public HttpContext HttpContext { get; }
+
+        public AspNetCoreDashboardContext(TemplateStorage storage, DashboardOptions options, HttpContext httpContext)
+        {
+            if (storage == null) throw new ArgumentNullException(nameof(storage));
+            if (options == null) throw new ArgumentNullException(nameof(options));
+
+            Storage = storage;
+            Options = options;
+
+            HttpContext = httpContext ?? throw new ArgumentNullException(nameof(httpContext));
+            Request = new AspNetCoreDashboardRequest(httpContext);
+            Response = new AspNetCoreDashboardResponse(httpContext);
+
+            if (!options.IgnoreAntiforgeryToken)
+            {
+                var antiforgery = HttpContext.RequestServices.GetService<IAntiforgery>();
+                var tokenSet = antiforgery?.GetAndStoreTokens(HttpContext);
+
+                if (tokenSet != null)
+                {
+                    AntiforgeryHeader = tokenSet.HeaderName;
+                    AntiforgeryToken = tokenSet.RequestToken;
+                }
+            }
+
+        }
+    }
+}
--- samples/AspNetCoreWebApplicationExample/Startup.cs
+++ samples/AspNetCoreWebApplicationExample/Startup.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using EmailTemplateLibrary;
 using EmailTemplateLibrary.Dashboard;
+using EmailTemplateLibrary.AspNetCore;
 
 namespace AspNetCoreWebApplicationExample
 {

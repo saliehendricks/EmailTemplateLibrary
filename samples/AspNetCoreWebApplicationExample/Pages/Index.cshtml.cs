fbba7f708a9bea43eb61ca6fef90872f4788833f
Salie Hendricks <saliehendricks@gmail.com>
2021-06-01 12:38:08 +0200

    Version fix

--- samples/AspNetCoreWebApplicationExample/Pages/Index.cshtml.cs
+++ samples/AspNetCoreWebApplicationExample/Pages/Index.cshtml.cs
@@ -5,21 +5,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmailTemplateLibrary.Storage;
 
 namespace AspNetCoreWebApplicationExample.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly TemplateStorage _templateStore;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        /// <summary>
+        /// Dependency for template storage is injected here
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="templateStore"></param>
+        public IndexModel(ILogger<IndexModel> logger, TemplateStorage templateStore)
         {
             _logger = logger;
+            _templateStore = templateStore;
         }
 
         public void OnGet()
         {
-
+            //access to templates
+            ViewData["Templates"] = _templateStore.GetTemplates();
         }
     }
 }
---
+++ src/EmailTemplateLibrary/Dashboard/Pages/LayoutPage.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace EmailTemplateLibrary.Dashboard.Pages
+{
+    partial class LayoutPage 
+    {
+        public string VersionText
+        {
+            get
+            {
+                var version = GetType().GetTypeInfo().Assembly.GetName().Version;
+                return $"{version.Major}.{version.Minor}.{version.Build}";
+            }
+        }
+    }
+}

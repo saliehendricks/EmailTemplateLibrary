0064db09ec3d9dfc08062d6b7f57c2f611eac15a
Salie Hendricks <saliehendricks@gmail.com>
2021-06-01 11:19:42 +0200

    Package info

---
+++ samples/AspNetCoreWebApplicationExample/IndexController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmailTemplateLibrary.Storage;
+
+namespace AspNetCoreWebApplicationExample
+{
+    public class IndexController : Controller
+    {
+        public IActionResult Index(TemplateStorage storage)
+        {
+            var templates = storage.GetTemplates();
+            ViewBag.Templates = templates;
+            return View();
+        }
+    }
+}

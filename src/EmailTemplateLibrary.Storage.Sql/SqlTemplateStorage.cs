8e0e510a4ca3ff5366374548023d0f12381b1721
Salie Hendricks <saliehendricks@gmail.com>
2021-06-13 22:03:01 +0200

    Sql unit tests, v1.0.2 bump releasetonuget

--- src/EmailTemplateLibrary.Storage.Sql/SqlTemplateStorage.cs
+++ src/EmailTemplateLibrary.Storage.Sql/SqlTemplateStorage.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using ServiceStack.OrmLite;
 using EmailTemplateLibrary.Storage.Base;
+using ServiceStack.OrmLite.Sqlite;
 
 namespace EmailTemplateLibrary.Storage.Sql
 {
     public class SqlTemplateStorage : SqlDbTemplateStorage
     {
-        public SqlTemplateStorage(SqlStorageOptions options) : base(options.ConnectionString, SqlServerDialect.Provider)
+        public SqlTemplateStorage(SqlStorageOptions options) : base(options.ConnectionString, options.ConnectionString == ":memory:" ? SqliteOrmLiteDialectProvider.Instance : SqlServerDialect.Provider)
         {}
     }
 
---
+++ tests/EmailTemplateLibrary.UnitTests/SqlContextTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmailTemplateLibrary.Storage.Sql;
+using EmailTemplateLibrary.Storage;
+
+namespace EmailTemplateLibrary.UnitTests
+{
+    [TestFixture]
+    public class SqlContextTest
+    {
+        [Test]
+        public void WhenUsingSqlExtension_ShouldCreateSqlStore()
+        {
+            //Arrange
+            var services = new ServiceCollection();
+            services.AddEmailTemplateLibraryServices(new Dashboard.TemplateDashboardOptions()
+            {
+            })
+            .AddSqlServerStorage(new SqlStorageOptions()
+            {
+                ConnectionString = ":memory:"
+            });
+
+            //Act
+            var provider = services.BuildServiceProvider();
+
+            //Assert
+            var templateStore = provider.GetService<TemplateStorage>();
+            Assert.That(templateStore.GetType(), Is.EqualTo(typeof(SqlTemplateStorage)), "SqlTemplateStore not injected");
+        }
+
+        [Test]
+        public void WhenUsingSqlExtension_ShouldCreateBaseTemplates()
+        {
+            //Arrange
+            var services = new ServiceCollection();
+            services.AddEmailTemplateLibraryServices(new Dashboard.TemplateDashboardOptions()
+            {
+            })
+            .AddSqlServerStorage(new SqlStorageOptions()
+            {
+                ConnectionString = ":memory:"
+            });
+
+            var provider = services.BuildServiceProvider();
+            var templateStore = provider.GetService<TemplateStorage>();
+
+            templateStore.CreateBaseTemplates();
+
+            //Act
+            var templates = templateStore.GetTemplates();
+
+            //Assert
+            Assert.That(templates, Is.Not.Null, "No templates created - null returned");
+            Assert.That(templates.Count, Is.GreaterThan(0), "No templates created");
+        }
+    }
+}
